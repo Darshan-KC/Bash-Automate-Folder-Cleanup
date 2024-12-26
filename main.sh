3bf8c79a46db93e823113044949d1f429f41e1ac
Darshan KC <darshanbahadurchhetri@gmail.com>
2024-12-26 18:31:01 +0545

    read arguments

--- main.sh
+++ main.sh
@@ -1,1 +1,11 @@
 # Automate folder clean up script
+#! /bin/bash
+
+# Script to clean up files older than a specified number of days from a folder.
+# Usage: ./cleanup.sh <folder_path> <days>
+
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <folder_path> <days>"
+    exit 1
+fi
